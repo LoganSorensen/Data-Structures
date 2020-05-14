e4f42f1aab791ebc17f45c146472e0c440cb0667
Logan Sorensen <loganmsorensen@gmail.com>
2020-05-14 14:48:09 -0700

    bst day 2 mvp

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -9,6 +9,66 @@ This part of the project comprises two days:
 2. Implement the `in_order_print`, `bft_print`, and `dft_print` methods
    on the BSTNode class.
 """
+from linked_list import LinkedList
+from bst_doubly_linked_list import DoublyLinkedList
+from collections import deque
+
+
+class Stack:
+    def __init__(self):
+        self.size = 0
+        # self.storage = []
+        self.storage = LinkedList()
+
+    def __len__(self):
+        # return len(self.storage)
+        return self.size
+
+    def push(self, value):
+        # return self.storage.append(value)
+        self.storage.add_to_end(value)
+        self.size += 1
+
+    def pop(self):
+        # if len(self.storage) == 0:
+        #     return None
+        # else:
+        #     return self.storage.pop()
+
+        if self.storage.head:
+            self.size -= 1
+            return self.storage.remove_from_end()
+
+
+class Queue:
+    def __init__(self):
+        self.size = 0
+        # self.storage = []
+        # self.storage = LinkedList()
+        self.storage = DoublyLinkedList()
+
+    def __len__(self):
+        # return len(self.storage)
+        return self.size
+
+    def enqueue(self, value):
+        # self.storage.append(value)
+        print(value)
+        self.size += 1
+        self.storage.add_to_tail(value)
+        
+
+    def dequeue(self):
+        # if len(self.storage) == 0:
+        #     return None
+        # else:
+        #     return self.storage.pop(0)
+        if self.storage.head:
+            self.size -= 1
+            return self.storage.remove_from_head()
+        return None
+
+
 class BSTNode:
     def __init__(self, value):
         self.value = value
@@ -25,7 +85,7 @@ class BSTNode:
         else:
             if self.right is None:
                 self.right = BSTNode(value)
-            else: 
+            else:
                 self.right.insert(value)
 
     # Return True if the tree contains the value
@@ -71,17 +131,47 @@ class BSTNode:
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
     def in_order_print(self, node):
-        pass
+
+        if node.left is not None:
+            node.in_order_print(node.left)
+
+        print(node.value)
+
+        if node.right is not None:
+            node.in_order_print(node.right)
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
     def bft_print(self, node):
-        pass
+        # create a new queue
+        queue = deque()
+        # add the current node to the queue
+        queue.append(node)
+        # run if the queue is not empty
+        while len(queue) > 0:
+            current = queue.popleft()
+            print(current.value)
+            # if there is a node to the left of the current node, add it to the queue
+            if current.left is not None:
+                queue.append(current.left)
+            # if there is a node to the right of the current node, add it to the queue
+            if current.right is not None:
+                queue.append(current.right)
+            
 
     # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal
     def dft_print(self, node):
-        pass
+        stack = Stack()
+        stack.push(node)
+        while len(stack) > 0:
+            current = stack.pop()
+            print(current.value)
+            if current.left is not None:
+                stack.push(current.left)
+            if current.right is not None:
+                stack.push(current.right)
+
 
     # Stretch Goals -------------------------
     # Note: Research may be required
@@ -93,3 +183,4 @@ class BSTNode:
     # Print Post-order recursive DFT
     def post_order_dft(self, node):
         pass
+
